3183c909a448d874a701fa0c8d4d26418c3a51f8
braysonwheeler <lbw98att@gmail.com>
2022-04-24 00:32:47 -0500

    Working gvm create target/ create task/start task functions

---
+++ create-and-scan-host-all-in-one.py
@@ -0,0 +1,37 @@
+from importlib.resources import path
+from ipaddress import ip_address
+from urllib import response
+from gvm.connections import UnixSocketConnection
+from gvm.protocols.gmp import Gmp
+from gvm.transforms import EtreeTransform
+from gvm.xml import pretty_print
+from lxml import etree
+import lxml
+
+connection = UnixSocketConnection(path=None) #Path=None is deafult gvmd.sock location
+transform = EtreeTransform()
+
+with Gmp(connection, transform=transform) as gmp:
+    gmp.authenticate('admin', '02169f70-676c-4789-8bad-8242fe85bd33')
+
+    #Create target to scan
+    name = f"Suspect Host 192.168.1.136" 
+    port_list = "4a4717fe-57d2-11e1-9a26-406186ea4fc5" #Default port list ID provided by openvas (generated when installing)
+    host_list=["192.168.1.136"] #Must be a list of strings
+    
+
+    response = gmp.create_target(name=name, hosts=host_list, port_list_id=port_list)
+    print(etree.tostring(response))
+    target_id = response.get('id')
+
+    #Create Task to scan the Target
+
+    full_and_fast_scan_config_id = 'daba56c8-73ec-11df-a475-002264764cea'
+    openvas_scanner_id = '08b69003-5fc2-4037-a479-93b440211c73'
+
+    name = "Scan Suspect Host 192.168.1.136"
+    response2 = gmp.create_task(name=name, config_id=full_and_fast_scan_config_id, target_id=target_id, scanner_id=openvas_scanner_id)
+    print(etree.tostring(response2))
+    task_id=response2.get('id')
+
+    #Start the scan
---
+++ gvm-start-task.py
@@ -0,0 +1,22 @@
+from importlib.resources import path
+from ipaddress import ip_address
+from urllib import response
+from gvm.connections import UnixSocketConnection
+from gvm.protocols.gmp import Gmp
+from gvm.transforms import EtreeTransform
+from gvm.xml import pretty_print
+from lxml import etree
+import lxml
+
+connection = UnixSocketConnection(path=None) #Path=None is deafult gvmd.sock location
+transform = EtreeTransform()
+
+def start_task(task_id):
+    with Gmp(connection, transform=transform) as gmp:
+        gmp.authenticate('admin', '02169f70-676c-4789-8bad-8242fe85bd33')
+        response = gmp.start_task(task_id)
+    print(etree.tostring(response))
+
+
+
+start_task("b194139a-7465-40ee-b880-64742ebfb8df")

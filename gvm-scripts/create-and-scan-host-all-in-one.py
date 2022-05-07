b5ad6c64ae99b758fca877f25e062a3ffa07e7df
root <root@2603-8080-2d01-a3a8-0a00-27ff-fe6b-a7a2.lan>
2022-05-07 03:06:20 -0500

    file structure

--- gvm-scripts/create-and-scan-host-all-in-one.py
+++ gvm-scripts/create-and-scan-host-all-in-one.py
@@ -17,10 +17,11 @@ with Gmp(connection, transform=transform) as gmp:
     gmp.authenticate('admin', '02169f70-676c-4789-8bad-8242fe85bd33')
     #CVE-1999-0497
     #Create target to scan
-    name = f"Suspect acutal Host 192.168.1.131 test" 
+    name = f"Suspect acutal Host 192.168.1.85 test" 
     #port_list = "4a4717fe-57d2-11e1-9a26-406186ea4fc5" #Default port list ID provided by openvas (generated when installing)
     port_list = "730ef368-57e2-11e1-a90f-406186ea4fc5"
-    host_list=["192.168.1.131"] #Must be a list of strings
+    #host_list holds the vulnerable machine we will scan
+    host_list=["192.168.1.85"] #Must be a list of strings
     
 
     response = gmp.create_target(name=name, hosts=host_list, port_list_id=port_list)
